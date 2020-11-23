5a6b8b6212362396e991b26ffec582e5dd943253
HunterAP23 <etgarkaspi@gmail.com>
2020-11-23 15:59:57 -0500

    Removed matplotlib, updated args and show video stats

--- video_analyser.py
+++ video_analyser.py
@@ -4,7 +4,6 @@ import sys
 import time
 
 import cv2
-from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -28,12 +27,23 @@ def main(args):
 
     frame_number = -1
 
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    size = (width, height)
+
+    result = None
+    if args.SAVE == 1 or args.SAVE == 3:
+        result = cv2.VideoWriter("original.avi", cv2.VideoWriter_fourcc(*"MJPG"), reported_fps, size)
+
     prev_frame = None
     while(cap.isOpened()):
         frame_number += 1
         progress(frame_number, total_frames, "Calculating frame")
 
         ret, frame = cap.read()
+
+        if result is not None:
+            result.write(frame)
 
         if frame_number == 0:
             prev_frame = frame.copy()
@@ -46,6 +56,8 @@ def main(args):
             frame_diffs.append(frame_diff)
 
             prev_frame = frame.copy()
+        except KeyboardInterrupt:
+            exit(1)
         except:
             break
 
@@ -53,11 +65,14 @@ def main(args):
             break
 
     cap.release()
+    if result is not None:
+        result.release()
     cv2.destroyAllWindows()
     progress(total_frames, total_frames, "Calculating frame")
 
     print("\nCalculating frame differences...")
     means = []
+    print(len(frame_diffs))
     for i in range(len(frame_diffs)):
         progress(i, len(frame_diffs), "Calculating mean for frame")
         means.append(frame_diffs[i].mean())
@@ -110,27 +125,78 @@ def main(args):
         temp_name = "".join(temp_name[0:-1])
         output_name = "{0}.csv".format(temp_name)
 
+    df = pd.DataFrame.from_dict(data)
     with open(output_name, "w", newline="\n") as csv_file:
-        df = pd.DataFrame.from_dict(data)
         df.to_csv(csv_file, index=False)
 
-    # if args.VIEW:
-    #     print("Displaying video containing only unique frames.")
-    #     cur_frame = 0
-    #     cap = cv2.VideoCapture(args.INPUT)
-    #     while(cap.isOpened()):
-    #         cur_frame += 1
-    #         if cur_frame <= len(res):
-    #             if cur_frame in res:
-    #                 ret, frame = cap.read()
-    #                 try:
-    #                     cv2.imshow("frame", frame)
-    #                     if cv2.waitKey(1) & 0xFF == ord("q"):
-    #                         break
-    #                 except:
-    #                     continue
-    #         else:
-    #             break
+    frametime_stats = pd.DataFrame(df, columns=["frametime"])
+    framerate_stats = pd.DataFrame(df, columns=["framerate"])
+
+    stats_basic = dict()
+    stats_frametime_dict = dict()
+    stats_framerate_dict = dict()
+
+    stats_basic["Number of Unique Frames"] = [int(sum(frames))]
+    stats_basic["Number of Duplicated Frames"] = [int(len(frames) - sum(frames))]
+    stats_basic["Percentage of Unique Frames"] = [sum(frames) / len(frames) * 100]
+    stats_basic["Percentage of Duplicated Frames"] = [stats_basic["Number of Duplicated Frames"][0] / len(frames) * 100]
+
+    stats_frametime_dict["Lowest"] = dict(frametime_stats.min(axis=0))
+    stats_frametime_dict["Highest"] = dict(frametime_stats.max(axis=0))
+    stats_frametime_dict["Mean"] = dict(frametime_stats.mean(axis=0))
+    stats_frametime_dict["Median"] = dict(frametime_stats.median(axis=0))
+    stats_frametime_dict["0.1 Percent Lows"] = dict(frametime_stats.quantile(q=0.001, axis=0))
+    stats_frametime_dict["1 Percent Lows"] = dict(frametime_stats.quantile(q=0.01, axis=0))
+    stats_frametime_dict["99 Percent Lows"] = dict(frametime_stats.quantile(q=0.99, axis=0))
+    stats_frametime_dict["99.9 Percent Lows"] = dict(frametime_stats.quantile(q=0.999, axis=0))
+
+    stats_framerate_dict["Lowest"] = dict(framerate_stats.min(axis=0))
+    stats_framerate_dict["Highest"]= dict(framerate_stats.max(axis=0))
+    stats_framerate_dict["Mean"] = dict(framerate_stats.mean(axis=0))
+    stats_framerate_dict["Median"] = dict(framerate_stats.median(axis=0))
+    stats_framerate_dict["0.1 Percent Lows"] = dict(framerate_stats.quantile(q=0.001, axis=0))
+    stats_framerate_dict["1 Percent Lows"] = dict(framerate_stats.quantile(q=0.01, axis=0))
+    stats_framerate_dict["99 Percent Lows"] = dict(framerate_stats.quantile(q=0.99, axis=0))
+    stats_framerate_dict["99.9 Percent Lows"] = dict(framerate_stats.quantile(q=0.999, axis=0))
+
+    stats_basic_df = pd.DataFrame.from_dict(stats_basic)
+    stats_frametime_df = pd.DataFrame.from_dict(stats_frametime_dict)
+    stats_framerate_df = pd.DataFrame.from_dict(stats_framerate_dict)
+
+    stats_joined = pd.concat([stats_frametime_df, stats_framerate_df], axis=0)
+
+    if args.SAVE > 1:
+        print("Saving video containing only unique frames.")
+        cur_frame = 0
+        cap = cv2.VideoCapture(args.INPUT)
+
+        result = None
+        if args.SAVE > 1:
+            result = cv2.VideoWriter("unique.avi", cv2.VideoWriter_fourcc(*"MJPG"), reported_fps, size)
+
+        while(cap.isOpened()):
+            cur_frame += 1
+            progress(cur_frame, len(frames), "Displaying frame")
+            if cur_frame in res:
+                ret, frame = cap.read()
+                try:
+                    if result is not None:
+                        result.write(frame)
+                except KeyboardInterrupt:
+                    exit(1)
+                except:
+                    break
+
+            # if cur_frame > len(frames):
+            #     break
+
+    cap.release()
+    if result is not None:
+        result.release()
+    cv2.destroyAllWindows()
+
+    print("\nStatistics", stats_basic_df.transpose().to_string(header=False))
+    print("\n", stats_joined.transpose().to_string())
 
 
 def parse_arguments():
@@ -143,17 +209,22 @@ def parse_arguments():
 
     threshold_help = "Pixel difference threshold to count as duplicate frames, must be an integer between 0 and 255.\n"
     threshold_help += "A value of 0 will count all all frames as unique, while 255 will only count\n"
-    threshold_help += "frames that are 100 percent different (Default is 5)."
+    threshold_help += "frames that are 100 percent different (Default: 5)."
     parser.add_argument("-t", "--threshold", dest="THRESHOLD", type=int, default=5, help=threshold_help)
 
-    # view_help = "Whether or not to display the final video with all the duplicated frames removed (default: False)"
-    # parser.add_argument("-v", "--view", dest="VIEW", action="store_true", default=False, help=view_help)
+    save_help = "Save the video frames of the original video as well as a version with duplicated frames removed.\n"
+    save_help += "A value of 0 will not save any video files.\n"
+    save_help += "A value of 1 will only save the original video file.\n"
+    save_help += "A value of 2 will only save the version with duplicated frames removed.\n"
+    save_help += "A value of 3 will save both the original and version with duplicated frames removed.\n"
+    save_help += "Note that saving the video file(s) can drastically increase the program's runtime. (Default: 0)"
+    parser.add_argument("-s", "--save", dest="SAVE", action="store", type=int, default=0, choices=[0, 1, 2, 3], help=save_help)
 
     args = parser.parse_args()
 
-    # if args.dpi <= 1:
-    #     parser.error("Value {0} for \"dpi\" argument was not a positive integer.".format(args.dpi))
-    #     exit(1)
+    if args.THRESHOLD < 0 or args.THRESHOLD > 255:
+        parser.error("Value {0} for \"threshold\" argument was not within the range of 0 to 255".format(args.THRESHOLD))
+        exit(1)
 
     return(args)
 
